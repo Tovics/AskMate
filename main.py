0851d216669a2e86a72a8b308a2b577ac3ebc258
Zsofia Radai <zsofiaradai@gmail.com>
2017-05-10 21:00:01 +0200

    q display

--- main.py
+++ main.py
@@ -5,12 +5,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def list_questions():
-    return render_template('list.html', question_list = import_from_file())
+    return render_template('list.html', question_list=import_from_file())
 
 
 @app.route('/addquestion')
 def render_question():
     return render_template('form.html')
+
+
+@app.route('/display', methods=['POST'])
+def display_question():
+    questions_list = import_from_file()
+
+    return render_template('display.html')
 
 
 @app.route("/question", methods=['POST'])
@@ -21,13 +28,13 @@ def add_question():
     if questions_list is None:
         question_ID = 1
     else:
-        question_ID = int(questions_list[len(questions_list)-1][0])+1
+        question_ID = int(questions_list[len(questions_list) - 1][0]) + 1
     if len(question_title) < 10:
         return render_template('form.html', msg='You should write longer question title! (Dumbass)')
     else:
         with open('questions.csv', 'a') as file_content:
-            file_content.write(str(question_ID) + ", " + question_title + '\n')
-        return render_template('list.html', question_list = import_from_file())
+            file_content.write(str(question_ID) + ", " + question_title + ", " + question_description + '\n')
+        return render_template('list.html', question_list=import_from_file())
 
 
 def import_from_file(file_name="questions.csv"):
@@ -42,6 +49,7 @@ def import_from_file(file_name="questions.csv"):
 def main():
     app.run(debug=True)
     import_from_file()
+
 
 if __name__ == '__main__':
     main()
