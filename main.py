821e287b2bf53a119bd5941f6bb7877759ee2c9a
Zsofia Radai <zsofiaradai@gmail.com>
2017-05-10 14:44:30 +0200

    write to file function added

--- main.py
+++ main.py
@@ -9,11 +9,18 @@ def hali():
 
 @app.route("/question", methods=['POST'])
 def add_question():
-    return render_template('form.html', msg = 'You should write longer idiot.')
+    question = request.form['question']
+    if len(question) < 10:
+        return render_template('form.html', msg='You should write longer idiot.')
+    else:
+        with open('questions.csv', 'a') as file_content:
+            file_content.write(question)
+        return render_template('form.html', msg='motherfucker')
 
 
 def main():
     app.run(debug=True)
+
 
 if __name__ == '__main__':
     main()
