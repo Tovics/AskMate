1348df7c12d9791f3bacec569c87dcce4f9457d0
Tovics <tovics1@gmail.com>
2017-05-10 17:55:25 +0200

    ID, export/import (in case of empty export file, IDexport drops error

---
+++ import_from_questions.py
@@ -0,0 +1,11 @@
+
+
+
+
+
+def main():
+    import_from_file()
+
+
+if __name__ == '__main__':
+    main()
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template
+import import_from_questions
 app = Flask(__name__)
 
 
@@ -9,18 +10,33 @@ def hali():
 
 @app.route("/question", methods=['POST'])
 def add_question():
-    question = request.form['question']
-    if len(question) < 10:
-        return render_template('form.html', msg='You should write longer idiot.')
+    question_title = request.form["question_title"]
+    question_description = request.form["question_description"]
+    questions_list = import_from_file()
+    if questions_list is None:
+        question_ID = 1
+    else:
+        question_ID = int(questions_list[len(questions_list)-1][0])+1
+    if len(question_title) < 10:
+        return render_template('form.html', msg='You should write longer question title! (Dumbass)')
     else:
         with open('questions.csv', 'a') as file_content:
-            file_content.write(question + '\n')
-        return render_template('form.html', msg='motherfucker')
+            file_content.write(str(question_ID) + ", " + question_title + '\n')
+        return render_template('form.html', msg=question_title)
+
+
+def import_from_file(file_name="questions.csv"):
+    questions_list = []
+    with open(file_name, "r") as file_content:
+        questions = file_content.readlines()
+    for i in questions:
+        questions_list.append(i.replace('\n', '').split(', '))
+    return questions_list
 
 
 def main():
     app.run(debug=True)
-
+    import_from_file()
 
 if __name__ == '__main__':
     main()
