90add9e02ed40d2206433ed33a4f27da95f24cc3
Zsofia Radai <zsofiaradai@gmail.com>
2017-05-23 18:02:05 +0200

    display question from database

--- cream.py
+++ cream.py
@@ -20,61 +20,49 @@ def render_question():
     return render_template('form.html')
 
 
-@app.route('/question/<question_ID>', methods=['GET', 'POST'])
-def display_question(question_ID):
-    filename = 'answer_' + str(question_ID) + '.csv'
-    questions_list = import_from_file()
-    if os.path.isfile(filename) is True:
-        answer_list = import_from_file(filename)
-    else:
-        answer_list = []
-    date = ""
-    answer = ""
-    for i in range(len(questions_list)):
-        if questions_list[i][0] == question_ID:
-            date = questions_list[i][3]
-            description = questions_list[i][2]
-            question_ID = questions_list[i][0]
-            question_title = questions_list[i][1]
+@app.route('/question/<question_id>', methods=['GET', 'POST'])
+def display_question(question_id):
+    questions_details = sql_queries.import_single_question_from_db(question_id)
+    date = questions_details[0][1]
+    title = questions_details[0][4]
+    question_id = questions_details[0][0]
+    answer = ''
+    question_description = questions_details[0][5]
+
     if request.method == 'POST':
         answer = request.form["answer"]
-        if os.path.isfile(filename) is True:
-            with open(filename, "a") as file_content:
-                file_content.write(answer + '\n')
-            return render_template('display.html', question_ID=question_ID, date=date, description=description, answer=answer, question_title=question_title, answer_list=answer_list)
-        else:
-            with open(filename, "w") as file_content:
-                file_content.write(answer + '\n')
-            return render_template('display.html', question_ID=question_ID, date=date, description=description, answer=answer, question_title=question_title, answer_list=answer_list)
-    return render_template('display.html', question_ID=question_ID, date=date, description=description, answer=answer, question_title=question_title, answer_list=answer_list)
+        return render_template('display.html', question_id=question_id, date=date, message=question_description, title=title)
+    else:
+        sql_queries.insert_answer(date, vote_number=0, answer='', image='')
+        return render_template('display.html', question_id=question_id, date=date, message=question_description, title=title)
 
 
 @app.route("/question", methods=['POST'])
 def add_question():
     question_title = request.form["question_title"]
     question_description = request.form["question_description"]
-    questions_list = import_from_file()
+    # questions_list = import_from_file()
     date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     if questions_list == []:
-        question_ID = 1
+        question_id = 1
     else:
-        question_ID = int(questions_list[len(questions_list) - 1][0]) + 1
+        question_id = int(questions_list[len(questions_list) - 1][0]) + 1
     if len(question_title) < 10:
         return render_template('form.html', msg='You should write longer question title! (Dumbass)')
     else:
         with open('questions.csv', 'a') as file_content:
-            file_content.write(str(question_ID) + ", " + question_title +
+            file_content.write(str(question_id) + ", " + question_title +
                                ", " + question_description + ", " + date + '\n')
         return render_template('list.html', question_list=import_from_file())
 
 
-def import_from_file(file_name="questions.csv"):
+"""def import_from_file(file_name="questions.csv"):
     questions_list = []
     with open(file_name, "r") as file_content:
         questions = file_content.readlines()
     for i in questions:
         questions_list.append(i.replace('\n', '').split(', '))
-    return questions_list
+    return questions_list"""
 
 
 def main():
--- sql_queries.py
+++ sql_queries.py
@@ -2,17 +2,48 @@ import psycopg2
 
 
 def import_questions_from_db():
-    connect_str = "dbname='borzfele' user='borzfele' host='localhost' password='91december30'"
+    connect_str = "dbname='zsofi' user='zsofi' host='localhost' password='pwd'"
     conn = psycopg2.connect(connect_str)
     conn.autocommit = True
     cursor = conn.cursor()
     cursor.execute("""SELECT * FROM question;""")
+    question_list = cursor.fetchall()
+    return question_list
+
+
+def import_answers_from_db(question_id):
+    connect_str = "dbname='zsofi' user='zsofi' host='localhost' password='pwd'"
+    conn = psycopg2.connect(connect_str)
+    conn.autocommit = True
+    cursor = conn.cursor()
+    # question_id = int(question_id)
+    cursor.execute("""SELECT * FROM answer WHERE id=%s;""", (question_id,))
+    answer = cursor.fetchall()
+    return answer
+
+
+def import_single_question_from_db(question_id):
+    connect_str = "dbname='zsofi' user='zsofi' host='localhost' password='pwd'"
+    conn = psycopg2.connect(connect_str)
+    conn.autocommit = True
+    cursor = conn.cursor()
+    cursor.execute("""SELECT * FROM question WHERE id=%s;""", (question_id,))
     question = cursor.fetchall()
     return question
 
 
+def insert_answer(date, vote_number, answer, image):
+    connect_str = "dbname='zsofi' user='zsofi' host='localhost' password='pwd'"
+    conn = psycopg2.connect(connect_str)
+    conn.autocommit = True
+    cursor = conn.cursor()
+    cursor.execute("""INSERT INTO answer (submission_time, vote_number, message, image)
+                            VALUES (%s, %s, %s, %s);""", (date, vote_number, answer, image))
+
+
 def main():
     import_questions_from_db()
+
 
 if __name__ == '__main__':
     main()
